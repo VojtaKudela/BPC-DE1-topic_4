a60ace0d95721f310c7dc0fdfb767e901b4bf30a
AntoninPutala <165297161+AntoninPutala@users.noreply.github.com>
2024-04-12 18:43:49 +0200

    Add files via upload

---
+++ DEMO/demo_ins.vhd
@@ -0,0 +1,307 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.04.2024 17:04:10
+-- Design Name: 
+-- Module Name: demo_ins - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity demo_ins is
+    Port ( 
+           step : in  STD_LOGIC_VECTOR (5 downto 0);
+           freq : out STD_LOGIC_VECTOR (3 downto 0);
+           vol  : out STD_LOGIC_VECTOR (3 downto 0)
+           );
+end demo_ins;
+
+architecture Behavioral of demo_ins is
+
+begin
+    melody_01 : process (step)is
+  begin
+    
+    case step is
+         when "000000" =>
+            freq <= x"8";
+            vol  <= x"F";
+            
+         when "000001" =>
+            freq <= x"B";
+            vol  <= x"F";
+         
+         when "000010" =>
+            freq <= x"5";
+            vol  <= x"F";
+         
+         when "000011" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "000100" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "000101" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "000110" =>
+            freq <= x"7";
+            vol  <= x"F";
+         
+         when "000111" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "001000" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "001001" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "001010" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "001011" =>
+            freq <= x"9";
+            vol  <= x"F";
+         
+         when "001100" =>
+            freq <= x"0";
+            vol  <= x"F";
+        
+         when "001101" =>
+            freq <= x"1";
+            vol  <= x"F";
+         
+         when "001110" =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+         when "001111" =>
+            freq <= x"1";
+            vol  <= x"F";
+         when "010000" =>
+            freq <= x"8";
+            vol  <= x"F";
+            
+         when "010001" =>
+            freq <= x"B";
+            vol  <= x"F";
+         
+         when "010010" =>
+            freq <= x"5";
+            vol  <= x"F";
+         
+         when "010011" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "010100" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "010101" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "010110" =>
+            freq <= x"7";
+            vol  <= x"F";
+         
+         when "010111" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "011000" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "011001" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "011010" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "011011" =>
+            freq <= x"9";
+            vol  <= x"F";
+         
+         when "011100" =>
+            freq <= x"0";
+            vol  <= x"F";
+        
+         when "011101" =>
+            freq <= x"1";
+            vol  <= x"F";
+         
+         when "011110" =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+         when "011111" =>
+            freq <= x"1";
+            vol  <= x"F";
+         
+         when "100000" =>
+            freq <= x"8";
+            vol  <= x"F";
+            
+         when "100001" =>
+            freq <= x"B";
+            vol  <= x"F";
+         
+         when "100010" =>
+            freq <= x"5";
+            vol  <= x"F";
+         
+         when "100011" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "100100" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "100101" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "100110" =>
+            freq <= x"7";
+            vol  <= x"F";
+         
+         when "100111" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "101000" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "101001" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "101010" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "101011" =>
+            freq <= x"9";
+            vol  <= x"F";
+         
+         when "101100" =>
+            freq <= x"0";
+            vol  <= x"F";
+        
+         when "101101" =>
+            freq <= x"1";
+            vol  <= x"F";
+         
+         when "101110" =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+         when "101111" =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+         when "110000" =>
+            freq <= x"8";
+            vol  <= x"F";
+            
+         when "110001" =>
+            freq <= x"B";
+            vol  <= x"F";
+         
+         when "110010" =>
+            freq <= x"5";
+            vol  <= x"F";
+         
+         when "110011" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "110100" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "110101" =>
+            freq <= x"8";
+            vol  <= x"F";
+         
+         when "110110" =>
+            freq <= x"7";
+            vol  <= x"F";
+         
+         when "110111" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "111000" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "111001" =>
+            freq <= x"4";
+            vol  <= x"F";
+         
+         when "111010" =>
+            freq <= x"0";
+            vol  <= x"F";
+         
+         when "111011" =>
+            freq <= x"9";
+            vol  <= x"F";
+         
+         when "111100" =>
+            freq <= x"0";
+            vol  <= x"F";
+        
+         when "111101" =>
+            freq <= x"1";
+            vol  <= x"F";
+         
+         when "111110" =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+         when others =>
+            freq <= x"1";
+            vol  <= x"F";
+            
+    end case;
+ end process melody_01;
+
+end Behavioral;
---
+++ DEMO/demo_main.vhd
@@ -0,0 +1,244 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.04.2024 17:17:03
+-- Design Name: 
+-- Module Name: demo_main - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity demo_main is
+    Port ( 
+           rst_play : in  STD_LOGIC;
+           rst      : in  STD_LOGIC;
+           clk      : in  STD_LOGIC;
+           demo_out : out STD_LOGIC_VECTOR (7 downto 0);
+           mode     : in  STD_LOGIC_VECTOR (1 downto 0)  -- two bits only please
+           );
+end demo_main;
+
+architecture Behavioral of demo_main is
+
+    component simple_counter
+        generic (
+           N : integer
+           );
+        port ( 
+           clk    : in  STD_LOGIC;
+           rst    : in  STD_LOGIC;
+           en     : in  STD_LOGIC;
+           count  : out STD_LOGIC_VECTOR (N-1 downto 0)
+           );
+    end component;
+    
+    component clock_enable
+        Generic (
+           PERIOD : integer
+           );
+        Port    ( 
+           clk   : in  STD_LOGIC;
+           rst   : in  STD_LOGIC;
+           pulse : out STD_LOGIC
+           );
+    end component;
+    
+    component freq_conv
+        Port ( 
+           bcd  : in  STD_LOGIC_VECTOR (3 downto 0);
+           freq : out STD_LOGIC_VECTOR (15 downto 0)
+           );
+    end component;
+    
+    component mux
+        generic (
+           NBIT : integer
+           );
+        Port ( 
+           in_0   : in STD_LOGIC_VECTOR (NBIT - 1 downto 0);
+           in_1   : in STD_LOGIC_VECTOR (NBIT - 1 downto 0);
+           in_2   : in STD_LOGIC_VECTOR (NBIT - 1 downto 0);
+           in_3   : in STD_LOGIC_VECTOR (NBIT - 1 downto 0);
+           sel    : in STD_LOGIC_VECTOR (1 downto 0);
+           output : out STD_LOGIC_VECTOR (NBIT - 1 downto 0));
+    end component;
+    
+    component tri_gen
+        Generic (
+           NBIT : integer;
+           FSMP : integer
+           );
+        Port ( 
+           clk  : in  STD_LOGIC;
+           freq : in  STD_LOGIC_VECTOR (15 downto 0);
+           vol  : in  STD_LOGIC_VECTOR (3 downto 0);
+           rst  : in  STD_LOGIC;
+           smpl : out STD_LOGIC_VECTOR (NBIT-1 downto 0)
+           );
+     end component;
+     
+     component demo_ins
+        Port ( 
+           step : in  STD_LOGIC_VECTOR (5 downto 0);
+           freq : out STD_LOGIC_VECTOR (3 downto 0);
+           vol  : out STD_LOGIC_VECTOR (3 downto 0)
+           );
+     end component;
+     
+     component demo_ins_1
+        Port ( 
+           step : in  STD_LOGIC_VECTOR (5 downto 0);
+           freq : out STD_LOGIC_VECTOR (3 downto 0);
+           vol  : out STD_LOGIC_VECTOR (3 downto 0)
+           );
+     end component;
+     
+     component demo_ins_2
+        Port ( 
+           step : in  STD_LOGIC_VECTOR (5 downto 0);
+           freq : out STD_LOGIC_VECTOR (3 downto 0);
+           vol  : out STD_LOGIC_VECTOR (3 downto 0)
+           );
+     end component;
+     
+     constant con_PERIOD   : integer := 2_000_000;
+     constant con_SMPL     : integer := 4160;
+     constant con_N        : integer := 6;
+     constant con_NBIT     : integer := 8;
+     constant con_FSMP     : integer := 12_000;
+     signal   sig_enable   : STD_LOGIC;
+     signal   sig_point    : STD_LOGIC_VECTOR (5 downto 0);
+     signal   sig_clk_tri  : STD_LOGIC;
+     signal   sig_freq_1   : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_vol_1    : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_freq_2   : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_vol_2    : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_freq_3   : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_vol_3    : STD_LOGIC_VECTOR (3 downto 0);
+     signal   sig_freq     : STD_LOGIC_VECTOR (3 downto 0); -- out mux
+     signal   sig_vol      : STD_LOGIC_VECTOR (3 downto 0); -- out mux
+     signal   sig_freq_tri : STD_LOGIC_VECTOR (15 downto 0);
+     
+begin
+    
+    NEXT_POINT : clock_enable
+        generic map (
+            PERIOD => con_PERIOD
+            )
+        port map (
+            rst   => rst_play,
+            clk   => clk,
+            pulse => sig_enable
+            );
+            
+      CNT_POINT : simple_counter
+        generic map (
+            N => con_N
+            )
+        port map (
+            rst   => rst_play,
+            clk   => clk,
+            en    => sig_enable,
+            count => sig_point
+            );
+            
+       DEMO_INS_01 : demo_ins
+        port map (
+            step  => sig_point,
+            freq  => sig_freq_1,
+            vol   => sig_vol_1
+            );
+            
+       DEMO_INS_02 : demo_ins_1
+        port map (
+            step  => sig_point,
+            freq  => sig_freq_2,
+            vol   => sig_vol_2
+            );
+            
+        DEMO_INS_3 : demo_ins_2
+         port map (
+            step  => sig_point,
+            freq  => sig_freq_3,
+            vol   => sig_vol_3
+            );
+            
+       MUX_FREQ   : mux
+        generic map (
+            NBIT   => 4
+            )
+        port map (
+            sel    => mode,
+            in_0   => sig_freq_2, -- demo_2 = state 100;
+            in_1   => sig_freq_3, -- demo_3 = state 101;
+            in_2   => x"0",
+            in_3   => sig_freq_1, -- demo_1 = state 011;
+            output => sig_freq
+            );
+                      
+        MUX_VOL   : mux
+         generic map (
+            NBIT   => 4
+            ) 
+         port map (
+            sel    => mode,
+            in_0   => sig_vol_2, -- demo_2 = state 100
+            in_1   => sig_vol_3, -- demo_3 = state 101;
+            in_2   => x"0",
+            in_3   => sig_vol_1, -- demo_1 = state 011;
+            output => sig_vol
+            );
+            
+        CL_TRI    : clock_enable
+          generic map (
+            PERIOD => con_SMPL
+            )
+          port map (
+            rst   => rst,
+            clk   => clk,
+            pulse => sig_clk_tri
+            );
+            
+        FREQ_CV    : freq_conv
+          port map (
+            bcd   => sig_freq,
+            freq  => sig_freq_tri
+            );
+            
+        GENERATOR  : tri_gen
+          generic map (
+            NBIT   => con_NBIT,
+            FSMP   => con_FSMP
+            ) 
+          port map (
+            clk    => sig_clk_tri,
+            rst    => rst,
+            freq   => sig_freq_tri,
+            vol    => sig_vol,
+            smpl   => demo_out
+            );
+
+end Behavioral;
